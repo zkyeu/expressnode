305ad9f952e545f1c9ff55324b8149a97ce36bf9
李亮 <zkyeu@163.com>
2021-12-24 01:20:35 +0800

    change table

--- models/types.js
+++ models/types.js
@@ -1,18 +1,18 @@
 /*
  * @Author: your name
  * @Date: 2021-08-10 15:50:55
- * @LastEditTime: 2021-08-19 10:25:53
+ * @LastEditTime: 2021-12-24 00:23:32
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /expressnode/models/user.js
  */
 const Base = require('./base');
 
-class Article extends Base {
+class Types extends Base {
   // 定义参数默认值为 user 表
-  constructor(props = 'types'){
+  constructor(props = 'tbl_types'){
     super(props);
   }
 }
 
-module.exports = new Article();
+module.exports = new Types();
