82b32291ec04c0c5ef95171a27c2215fb1b05458
李亮 <liliang@liliangdeMacBook-Pro.local>
2022-01-25 10:01:25 +0800

    feat: config

--- models/knex.js
+++ models/knex.js
@@ -1,12 +1,12 @@
 /*
  * @Author: your name
  * @Date: 2021-08-10 15:42:57
- * @LastEditTime: 2022-01-04 17:07:10
+ * @LastEditTime: 2022-01-25 09:59:36
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /expressnode/models/knex.js
  */
-const configs = require('../config');
+const configs = require('../config/config');
 // 把配置文件中的信息，设置在初始化配置中
 module.exports = require('knex')({
   client: 'mysql',
@@ -16,12 +16,12 @@ module.exports = require('knex')({
     user: configs.mysql.user,
     password: configs.mysql.password,
     database: configs.mysql.database,
-    charset: 'utf8mb4', 
+    charset: 'utf8mb4'
   },
   // 打印错误
   log: {
-    error (message) {
-      console.log('[knex error]', message)
+    error(message) {
+      console.log('[knex error]', message);
     }
   }
 });
