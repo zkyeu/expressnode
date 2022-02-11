7670536f37846636ef677cb04114d8d4ab0989bc
liliang <zkyeu@163.com>
2022-02-11 11:53:20 +0800

    分页

--- controllers/list.js
+++ controllers/list.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-08-10 15:55:34
- * @LastEditTime: 2022-01-24 23:54:17
+ * @LastEditTime: 2022-02-11 11:51:47
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /expressnode/controllers/user.js
@@ -10,44 +10,47 @@
 const item = require('../models/article.js');
 const Util = require('../utils/utils.js');
 const listController = {
-  list: async function(req,res,next){
-    let {pn, rn, id} = req.body;
+  list: async function (req, res, next) {
+    let { pn, rn, id, origin } = req.body;
     // console.log(pn, rn,req.body);
-    try{
+    try {
       let condition = {
-        'deleted': 0 // 未删除
+        deleted: 0 // 未删除
       };
-      
-      if(id) {
-        condition['type'] = id; // 判断文章分类
-      }
-      let list = await item
-      .selects(['*'], { ...condition }, { pn: pn, rn: rn })
-      .orderBy([{
-        column: 'create_time',
-        order: 'desc'
-      }]);
 
-      let lists = await item.count('title as cnt',{'deleted': 0}).first();
-      let obj = list.map((item,index) => {
-        item['create_time'] = Util.formatDate(item['create_time']);
-        return {
-          ...item,
-          operate: [
-            {
-              name: '查看',
-              type: 'view',
-            },
-            {
-              name: '编辑',
-              type: 'edit',
-            },
-            {
-              name: '删除',
-              type: 'delete',
-            }
-          ]
+      if (id) condition['type'] = id; // 判断文章分类
+
+      let list = await item.selects(['*'], { ...condition }, { pn: pn, rn: rn }).orderBy([
+        {
+          column: 'create_time',
+          order: 'desc'
         }
+      ]);
+
+      let lists = await item.count('title as cnt', { ...condition }).first(); // 条目统计
+      let obj = list.map((item, index) => {
+        item['create_time'] = Util.formatDate(item['create_time']);
+        let json =
+          parseInt(origin) === 1
+            ? item
+            : {
+                ...item,
+                operate: [
+                  {
+                    name: '查看',
+                    type: 'view'
+                  },
+                  {
+                    name: '编辑',
+                    type: 'edit'
+                  },
+                  {
+                    name: '删除',
+                    type: 'delete'
+                  }
+                ]
+              };
+        return json;
       });
       // let list = await item
       // .selects(['*'], {'deleted': 0}, {pn:pn, rn:rn })
@@ -57,14 +60,14 @@ const listController = {
       // }]);
       res.json({
         errNo: 0,
-        message: "操作成功",
+        message: '操作成功',
         data: obj,
         count: lists.cnt
-      })
-    }catch(e){
-      res.json({ errNo: 1, message: "操作失败", data: e })
+      });
+    } catch (e) {
+      res.json({ errNo: 1, message: '操作失败', data: e });
     }
   }
-}
+};
 
 module.exports = listController;
--- models/base.js
+++ models/base.js
@@ -1,54 +1,58 @@
 /*
  * @Author: your name
  * @Date: 2021-08-10 15:49:20
- * @LastEditTime: 2022-01-04 13:48:48
+ * @LastEditTime: 2022-02-11 11:53:06
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /expressnode/models/base.js
  */
 const knex = require('../models/knex');
 
-class Base{
-  constructor(props){
+class Base {
+  constructor(props) {
     this.table = props;
   }
- 
+
   // 查找
-  all () {
+  all() {
     // console.log('all');
     return knex(this.table).select().limit(10);
   }
   // select * from `article` left join `articlebody` on `article`.`contentid` = `articlebody`.`aid`
   // 拼接查找
   leftJoinFind(id) {
-    return knex(this.table).leftJoin('tbl_article_detail','tbl_article.article_id', 'tbl_article_detail.aid').where({ id }).first();
+    return knex(this.table)
+      .leftJoin('tbl_article_detail', 'tbl_article.article_id', 'tbl_article_detail.aid')
+      .where({ id })
+      .first();
   }
 
   //按需查找
-  find (id) {
+  find(id) {
     return knex(this.table).where({ id }).first();
   }
 
-  selects (parmas, condition, pages) {
+  selects(parmas, condition, pages) {
     // console.log(condition);
     return knex(this.table)
-    .column(parmas)
-    .select()
-    .where(condition)
-    .limit(pages.rn).offset((pages.rn * (pages.pn - 1)));
+      .column(parmas)
+      .select()
+      .where(condition)
+      .limit(pages.rn)
+      .offset(pages.rn * (pages.pn - 1));
   }
 
   // 新增
-  insert (params){
+  insert(params) {
     return knex(this.table).insert(params);
   }
 
   // 更改
-  update (id, params){
+  update(id, params) {
     // console.log(id, params);
     // delete params.id;
     // console.log(this.table);
-     return knex(this.table).where(id).update(params);
+    return knex(this.table).where(id).update(params);
     // if (this.table ==='tbl_article_detail'  || this.table ==='tbl_article') {
     //   return knex(this.table).where(id).update(params); // 文章成功
     // } else {
@@ -57,7 +61,7 @@ class Base{
   }
 
   // 删除
-  delete (id){
+  delete(id) {
     return knex(this.table).where('id', '=', id).del();
   }
   // 计数
